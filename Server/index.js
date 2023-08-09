c29996d7376f52b7b9d8fd7e8afe60698253cbba
fernandoiosono <fernandoiosono@gmail.com>
2023-08-08 22:01:08 -0600

    Server Creation Is Now Modularized

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,20 @@
-const router = require('./routes/index.js');
-const express = require("express");
-const morgan = require("morgan");
+const server = require('./src/server.js');
+const router = require('./src/routes/index.js');
+
 const cors = require('cors');
+const morgan = require('morgan');
 
 const PORT = 3001;
-const server = express();
 
-server.use(morgan("dev"));
 server.use(cors());
+server.use(morgan("dev"));
+server.use('/rickandmorty', router);
+
+server.listen(PORT, () => {
+	console.log(`Server raised in port: ${PORT}`);
+});
+
+////////////////////////////////////////////////////////
 // server.use((req, res, next) => {
 // 	res.header("Access-Control-Allow-Origin", "*");
 // 	res.header("Access-Control-Allow-Credentials", "true");
@@ -15,13 +22,8 @@ server.use(cors());
 // 	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 // 	next();
 // });
-server.use(express.json());
-server.use('/rickandmorty', router);
 
-server.listen(PORT, () => {
-	console.log(`Server raised in port: ${PORT}`);
-});
-
+////////////////////////////////////////////////////////
 // const http = require("http");
 // const getCharByID = require("./controllers/getCharByID.js");
 
