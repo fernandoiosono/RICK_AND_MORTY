becdeee5b91e62dd4e1fafa439d52ed7ccee2001
fernandoiosono <fernandoiosono@gmail.com>
2023-08-11 23:19:58 -0600

    Server Listen Now After Database Sync

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-
 const cors = require('cors');
 const morgan = require('morgan');
 const server = require('./src/server.js');
+const { database } = require('./src/database/database.js')
 
 const { handleUsers, 
 	handleFavorites, 
@@ -13,10 +13,14 @@ const { LOCALHOST_PORT } = process.env;
 server.use(cors());
 server.use(morgan("dev"));
 
-server.use('/rickandmorty/user', handleUsers);
-server.use('/rickandmorty/favorite', handleFavorites);
-server.use('/rickandmorty/character', handleCharacters);
+server.use('/rickandmorty/users', handleUsers);
+server.use('/rickandmorty/favorites', handleFavorites);
+server.use('/rickandmorty/characters', handleCharacters);
 
-server.listen(LOCALHOST_PORT, () => {
-	console.log(`Server raised in port: ${LOCALHOST_PORT}`);
-});
+database.sync({ force: true })
+	.then(() => {
+		server.listen(LOCALHOST_PORT, () => {
+			console.log(`Server raised in port: ${LOCALHOST_PORT}`);
+		});
+	})
+	.catch((error) => console.log(error));
