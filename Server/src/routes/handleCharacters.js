8c2039242787678069b869d129f9b964ee983702
fernandoiosono <fernandoiosono@gmail.com>
2023-08-25 02:04:56 -0600

    Updates

--- Server/src/routes/handleCharacters.js
+++ Server/src/routes/handleCharacters.js
@@ -9,4 +9,11 @@ router.get('/:id', errorHandler(async (req, res) => {
     res.status(200).json(character);
 }));
 
+router.post('/', errorHandler(async (req, res) => {
+    const { id } = req.params;
+    const character = await getCharByID(id);
+
+    res.status(200).json(character);
+}));
+
 module.exports = router;
